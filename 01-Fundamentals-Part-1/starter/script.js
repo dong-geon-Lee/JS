6db82c30681a6b489e0d37813a5bec48ebce57db
dong-geon-Lee <ch9716@naver.com>
2021-11-07 13:24:00 +0900

    section1~section2 learning

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,111 @@
+// let js = "amazing";
+// console.log(1 + 1);
+
+// console.log("lee");
+// console.log(23);
+
+// let firstName = "me";
+// console.log(firstName);
+
+// const PI = 3.1415;
+// console.log(PI);
+
+// TEST -----------------------------------------------------
+//? LECTURE: Values and Variables
+
+// let country = "대한민국";
+// let continent = "인천 계양구";
+// let population = "5000만";
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// ----------------------------------------------------------
+
+// console.log(true);
+// console.log(typeof true);
+// console.log(typeof false);
+// console.log(typeof "bo");
+// console.log(typeof 3);
+
+// country = 2;
+// console.log(country);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// TEST -----------------------------------------------------
+//? LECTURE: Data Types
+
+// let isIsland1 = true;
+// let language;
+
+// console.log(typeof isIsland1);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+
+// ----------------------------------------------------------
+
+// let age = 30;
+// age = 31;
+// console.log(age);
+
+// const birthYear = 1991;
+
+// TEST -----------------------------------------------------
+//? LECTURE: let, const and var
+
+// const language = "한글";
+// console.log(language);
+// language = "이런?";
+// ----------------------------------------------------------
+
+// const lee = 1;
+// const lee2 = 2;
+// const sum = lee + lee2;
+
+// console.log(lee, lee2);
+// console.log(sum, 2 ** 3, 10 / 5, 10 % 5);
+
+// const firstName = "lee";
+// const lastName = "dong-geon";
+// console.log(firstName + " " + lastName);
+// console.log(`${firstName} ${lastName}`);
+
+// let x = 10 + 5;
+// x += 5;
+// x *= 2;
+// x /= 10;
+// x++; // x = x + 1;
+// x--;
+// console.log(x);
+
+// const now = 2037;
+// const age1 = 2037;
+// const age2 = 2037;
+
+// let x, y;
+// x = y = 25 - 10 - 5;
+// console.log(x, y);
+
+// const avAge
+
+// #1 코딩테스트
+
+const markMass = 78;
+const markHeight = 1.69;
+
+const johnMass = 92;
+const johnHeight = 1.95;
+
+const BMI1 = markMass / markHeight ** 2;
+const BMI2 = johnMass / johnHeight ** 2;
+
+const markHigherBMI = BMI1 > BMI2;
+
+console.log(BMI1);
+console.log(BMI2);
+console.log(markHigherBMI, "point");
